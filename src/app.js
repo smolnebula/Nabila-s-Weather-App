b6ea9ff2b2b7cc8deecde5495a081c05854582b7
smolnebula <104291297+smolnebula@users.noreply.github.com>
2022-07-28 19:42:13 -0400

    JS: Added API for live temperature data for searched city

--- src/app.js
+++ src/app.js
@@ -39,3 +39,40 @@ if (currentMinutes < 10) {
 }
 let todaysTime = document.querySelector("li .current-time");
 todaysTime.innerHTML = `${currentHour}:${currentMinutes}`;
+
+//API for live weather data
+let endPoint = "https://api.openweathermap.org/data/2.5/weather?";
+let apiKey = "ff3837d74098813bfcef9f731c3749cf";
+let units = "imperial";
+
+// Give searched city's weather data
+function displayWeatherCondition(response) {
+  let currentCity = document.querySelector("li .current-city");
+  currentCity.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+//Change city
+let searchInput = document.querySelector("#search-input");
+let searchForm = document.querySelector("#search-bar");
+console.log(searchForm);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+
+searchForm.addEventListener("submit", handleSubmit);
